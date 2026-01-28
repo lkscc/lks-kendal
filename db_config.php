d74a02a20edffa446b03080aab5284a5ea1810e5
lkscc <iot@smksakti.sch.id>
2026-01-28 19:02:54 +0700

    Create db_config.php

---
+++ db_config.php
@@ -0,0 +1,7 @@
+<?php
+// Peserta wajib mengisi variabel di bawah ini sesuai hasil konfigurasi RDS
+define('DB_SERVER', 'ENDPOINT_RDS_ANDA'); 
+define('DB_USERNAME', 'admin_lks');
+define('DB_PASSWORD', 'PasswordPeserta123');
+define('DB_NAME', 'lks_db');
+?>
